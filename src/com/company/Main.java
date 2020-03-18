f682e6b2460084e0620cfc599b2608aa63619e27
chinoportela <matiasportela94@gmail.com>
2020-03-18 20:59:19 -0300

    pasando en limpio

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World");
-        System.out.println("Cara de verga");
-        System.out.println("Vos cara de verga");
     }
 }
